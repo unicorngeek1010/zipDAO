99f20148093856c9e7caba499f6fd99b2a2fb63f
devStack03 <devking0325@gmail.com>
2022-09-15 16:06:10 +0900

    build production for deploy

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import productRoutes from "./routes/index.js";
 import cors from "cors";
-
+import path from 'path';
 const app = express();
 
 try {
@@ -13,6 +13,10 @@ try {
 app.use(cors());
 app.use(express.urlencoded());
 app.use(express.json());
+const __dirname = path.resolve(path.dirname(''));
+const buildPath = path.join(__dirname, '..', 'production');
+console.log(buildPath);
+app.use(express.static(buildPath));
 app.use('/products', productRoutes);
 
 app.listen(5000, () => console.log('Server running at port 5000'));
